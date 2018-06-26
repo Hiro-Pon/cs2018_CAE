ba47c42191a95728309c4b4ba8e4a6dad0b7ee13
Hiroya NAKAJIMA <hiroya.nakajima@gmail.com>
2018-06-27 00:57:56 +0900

    0:57

--- CAE_11/led7seg9.v
+++ CAE_11/led7seg9.v
@@ -1,14 +1,15 @@
-module led7seg (ILED,SA);
-    input [3:0]a;
-    output [7:0]LED,SA;
+module led7seg9 (I,LED,SA);
+    input [3:0]I;
+    output [7:0]LED;
+    output [3:0]SA;
 
     assign  SA = 4'bZZZ0;
-    assign  LED[0] = ;
-    assign  LED[1] = ;
-    assign  LED[2] = ;
-    assign  LED[3] = ;
-    assign  LED[4] = ;
-    assign  LED[5] = ;
-    assign  LED[6] = ;
+    assign  LED[0] = (~I[0] & I[1] & I[2] & I[3] )^(I[0] & I[1] & ~I[2] & I[3])^(I[0] & ~I[1] & ~I[2] & I[3]);
+    assign  LED[1] = (~I[0] & I[1] & ~I[2] & I[3])^(I[0] & ~I[1] & ~I[2] & I[3]);
+    assign  LED[2] = I[0] & ~I[1] &I[2] & I[3];
+    assign  LED[3] = (~I[0] & I[1] &I[2] & I[3])^(I[0] & I[1] & ~I[2] & I[3])^(~I[0] & I[1] &I[2] & ~I[3]);
+    assign  LED[4] = (~I[0] & I[1]) ^ (~I[0] & ~I[1]) ^ (I[0] & I[1] & ~I[2] & I[3]);
+    assign  LED[5] = (~I[0] & I[1] &I[2] & I[3]) ^ (~I[0] & ~I[1]) ^ (~I[1] & I[2] & I[3]);
+    assign  LED[6] = I[1];
     assign  LED[7] = 1'b1;
 endmodule
--- CAE_11/test_leg7seg9.v
+++ CAE_11/test_leg7seg9.v
@@ -1,16 +1,28 @@
 module test_led7seg;
-reg a ,b;
-wire [7:0]LED,SA;
-led7seg ma1 (a, b, LED,SA);
+reg [3:0]I;
+wire [7:0]LED;
+wire [3:0]SA;
+led7seg9 ma1 (I, LED,SA);
 initial begin
     $dumpfile("test_led7seg.vcd");
     $dumpvars(0, test_led7seg);
-    $monitor ("%t: a =%b, b =%b, LED[0] =%b, LED[1] =%b, LED[2] =%b, LED[3] =%b, LED[4] =%b, LED[5] =%b, LED[6] =%b", $time, a, b, LED[0],LED[1], LED[2], LED[3], LED[4], LED[5], LED[6]);
-    a = 0; b = 0;
-    #10 a = 1;
-    #10 a = 0; b = 1;
-    #10 a = 1;
-    #10 a = 0; b = 0;
+    $monitor ("%t: I =%b, LED[0] =%b, LED[1] =%b, LED[2] =%b, LED[3] =%b, LED[4] =%b, LED[5] =%b, LED[6] =%b", $time, I, LED[0],LED[1], LED[2], LED[3], LED[4], LED[5], LED[6]);
+    I = 4'b1111;
+    #10 I = 4'b1110;
+    #10 I = 4'b1101;
+    #10 I = 4'b1100;
+    #10 I = 4'b1011;
+    #10 I = 4'b1010;
+    #10 I = 4'b1001;
+    #10 I = 4'b1000;
+    #10 I = 4'b0111;
+    #10 I = 4'b0110;
+    #10 I = 4'b0101;
+    #10 I = 4'b0100;
+    #10 I = 4'b0011;
+    #10 I = 4'b0010;
+    #10 I = 4'b0001;
+    #10 I = 4'b0000;
     #10 $finish;
 end
 endmodule
